6232cb229cd53bca696ac7f603615bd7fcdacf5a
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2022-09-19 20:07:00 +0100

    Lesson 02: JavaScript Introduction;: Variables and basic types

---
+++ lesson02/HelloWorld.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var a: Int = 10
+    a = "SLB"
+    println(a.length)
+
+    println("Olá Glorioso mundo onde o Maior clube do mundo existe e tem mais títulos que todos os outros")
+}
