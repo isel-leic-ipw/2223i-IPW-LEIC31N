998d183e789f3cb0b0802c76c1cdda8553eaa8b8
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2022-11-04 18:22:50 +0000

    Lesson 14: HTTP server application using Express module. Tasks management Web application (inicial version)

---
+++ lesson14/tasks-server-simple.mjs
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const PORT = 1904
+
+console.log("Start setting up server")
+let app = express()
+
+app.get('/tasks', function(req, rsp) {
+    console.log(req.query)
+    rsp.end(`GET tasks with query ${req.query.q}`)
+})
+
+app.get('/tasks/:id', function(req, rsp) {
+    rsp.end(`GET task with id ${req.params.id}`)
+})
+
+app.delete('/tasks/:id', function(req, rsp) {
+    rsp.end(`DELETE task with id ${req.params.id}`)
+})
+
+app.post('/tasks', function(req, rsp) {
+    rsp.end("POST task")
+})
+
+app.put('/tasks/:id', function(req, rsp) {
+    rsp.end(`PUT task with id ${req.params.id}`)
+})
+
+app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
+
+console.log("End setting up server")
