3a0f357fa7902e8cf247ff9635eb73318bebee70
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2022-11-11 18:30:16 +0000

    Lesson 17: Tasks server with open API documentation available on the application, under /api-docs path

--- lesson17/tasks-server.mjs
+++ lesson17/tasks-server.mjs
@@ -3,12 +3,18 @@
 
 import express from 'express'
 import * as api from './tasks-http-api.mjs'
+import swaggerUi from 'swagger-ui-express'
+import yaml from 'yamljs'
+import cors from 'cors'
 
+const swaggerDocument = yaml.load('./docs/tasks-api.yaml')
 const PORT = 1904
 
 console.log("Start setting up server")
 let app = express()
 
+app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
 
 app.get('/tasks', api.getTasks)
