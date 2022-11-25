6184f550347ed12149b31d8dc7610b13631fecf3
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2022-11-25 13:02:33 +0000

    Lesson 21: Tasks application refactored to include browser web interface (HTML+CSS).

--- lesson21/tasks-server.mjs
+++ lesson21/tasks-server.mjs
@@ -1,6 +1,7 @@
 // Application Entry Point. 
 // Register all HTTP API routes and starts the server
 
+
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import yaml from 'yamljs'
@@ -9,13 +10,15 @@ import cors from 'cors'
 import * as tasksData from './data/tasks-data-mem.mjs'
 import * as usersData from './data/users-data.mjs'
 import tasksServicesInit from './services/tasks-services.mjs'
-import apiInit from './api/tasks-http-api.mjs'
+import apiInit from './web/api/tasks-http-api.mjs'
+import siteInit from './web/site/tasks-http-site.mjs'
 
 const swaggerDocument = yaml.load('./docs/tasks-api.yaml')
 const PORT = 1904
 
 const tasksServices = tasksServicesInit(tasksData, usersData)
 const api = apiInit(tasksServices)
+const site = siteInit(tasksServices)
 
 console.log("Start setting up server")
 let app = express()
@@ -24,11 +27,16 @@ app.use(cors())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json())
 
-app.get('/tasks', api.getTasks)
-app.get('/tasks/:id', api.getTask)
-app.delete('/tasks/:id', api.deleteTask)
-app.post('/tasks', api.createTask)
-app.put('/tasks/:id', api.updateTask)
+// Web site routes
+app.get('/site/index', site.getIndex)
+
+
+// Web api routes 
+app.get('/api/tasks', api.getTasks)
+app.get('/api/tasks/:id', api.getTask)
+app.delete('/api/tasks/:id', api.deleteTask)
+app.post('/api/tasks', api.createTask)
+app.put('/api/tasks/:id', api.updateTask)
 
 app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
 
